76e038c32c24e98d7fa748d70a396b912eeffe0e
venexdevelopment <venexstoreacc@gmail.com>
2025-06-28 23:14:42 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,25 @@
+import React from "react"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import Cassino from "./pages/Cassino"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
 
+const App = () => {
+  return (
+    <div className="bg-zinc-900 text-white min-h-screen flex flex-col font-sans">
+      <Router>
+        <Header />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/cassino" element={<Cassino />} />
+          </Routes>
+        </main>
+        <Footer />
+      </Router>
+    </div>
+  )
+}
+
+export default App
